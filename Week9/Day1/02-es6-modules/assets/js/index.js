21cc3587c5023af2193f0c1faff9cc89cafd2e4e
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2022-08-29 15:54:07 -0700

    added solutions dir to day1 and pushed all solutions for the day

--- Week9/Day1/02-es6-modules/assets/js/index.js
+++ Week9/Day1/02-es6-modules/assets/js/index.js
@@ -1,7 +1,4 @@
-// import { Game } from "./game.js";
-import Game from "./game.js";
-
 window.onload = () => {
-  const game = new Game();
-  game.start();
+    const game = new Game();
+    game.start();
 };
